1910e348d73809f75addc317c0d6b071dd57f26a
rafafloresok <rafafloresok@gmail.com>
2022-03-10 13:11:05 -0300

    se mejora estilo y se agrega boton enviar pedido

--- script/script.js
+++ script/script.js
@@ -3,9 +3,12 @@ let addButtons = document.querySelectorAll(".agregar");
 let orderList = document.querySelector("ol");
 let showOrderBtn = document.querySelector(".btn-info");
 let orderContainer = document.querySelector(".container--pedido");
+let counter = document.querySelector(".contador");
+let count = 0;
+let sendOrder = document.querySelector(".enviar");
 
 /* CLASE PARA CONSTRUIR ITEMS DEL PEDIDO */
-class orderItem {
+class OrderItem {
     constructor(item, precio) {
         this.crearDiv(item, precio);
     }
@@ -18,7 +21,7 @@ class orderItem {
         /* CREAR EL BOTON PARA REMOVER DEL PEDIDO */
         let removeBtn = document.createElement("span");
         removeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-octagon-fill" viewBox="0 0 16 16"><path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zm-6.106 4.5L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/></svg>`;
-        removeBtn.classList.add("badge","bg-primary");
+        removeBtn.classList.add("eliminar","badge","bg-danger");
 
         /* CREAR EL ITEM PARA EL PEDIDO */
         let listItem = document.createElement("li");
@@ -34,21 +37,33 @@ class orderItem {
         /* AGREGAR FUNCIONALIDAD AL BOTON PARA REMOVER DEL PEDIDO */
         removeBtn.addEventListener("click", function () {
             listItem.remove();
+            count--
+            counter.textContent = count;
         })
         
     }
 }
+
+
 
 /* AGREGAR FUNCIONALIDAD A TODOS LOS BOTONES PARA AGREGAR AL PEDIDO */
 for (let i = 0; i < addButtons.length; i++) {
     addButtons[i].addEventListener("click", function() {
         let item = this.parentElement.childNodes[1].textContent;
         let precio = this.parentElement.childNodes[3].textContent;
-        new orderItem (item,precio);
+        new OrderItem (item,precio);
+        count++
+        counter.textContent = count;
     })
 }
 
 /* AGREGAR FUNCIONALIDAD AL BOTON MOSTRAR PEDIDO */
 showOrderBtn.addEventListener("click", function () {
-    orderContainer.classList.toggle("display-none")
+    orderContainer.classList.toggle("display-none");
+    sendOrder.classList.toggle("display-none");
+    if (this.textContent == "Ver pedido") {
+        this.textContent = "Ocultar pedido";
+    } else {
+        this.textContent = "Ver pedido"
+    }
 })
